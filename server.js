06c36985ee83ad832c5402e84f363e96d2f9ee01
Kai Jauslin <kai.jauslin@cando-image.com>
2017-02-03 23:07:21 +0100

    Made ttn auth key configurable (excluded from git)

---
+++ server.js
@@ -0,0 +1,25 @@
+var httpProxy = require('http-proxy');
+var express = require('express');
+var app = express();
+
+/**
+ * curl -X GET --header 'Accept: application/json'
+ * --header 'Authorization: key ttn-account-v2.HzLYlXzyjFkNEl5y7oljbfOGzoYPZoRFHCRApkRgkZg'
+ * 'https://rollingsparks.data.thethingsnetwork.org/api/v2/query'
+ */
+var proxy = httpProxy.createProxyServer();
+app.all("/api/*", function(req,res) {
+  proxy.web(req, res, {
+    target: "https://rollingsparks.data.thethingsnetwork.org/",
+    secure: true,
+    changeOrigin: true,
+    headers: {
+      'Authorization': 'key ttn-account-v2.HzLYlXzyjFkNEl5y7oljbfOGzoYPZoRFHCRApkRgkZg',
+    }
+  });
+});
+
+app.use(express.static('frontend/___dist'));
+var server = app.listen(3000, function() {
+  console.log('Express is listening to http://localhost:3000');
+});
